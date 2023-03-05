038ed29dc2ddbf5511696764c461adea3199de0b
Miles <mlsmzk02@gmail.com>
2023-03-05 01:33:51 -0500

    Finish code for quizzes assignment

---
+++ h5-quizzes/questions.js
@@ -0,0 +1,30 @@
+var questions = [ 
+    {Q: "Who starred in Casablanca?",
+        A: "Charleton Heston",
+        B: "Matt Damon",
+        C: "Harrison Ford",
+        D: "Humphrey Bogart",
+        ANS: "D"
+    },
+    {Q: "Who won the most Academy Awards (Oscars) for best actress?",
+        A: "Meryl Streep",
+        B: "Emma Thompson",
+        C: "Katharine Hepburn",
+        D: "Ingrid Bergman",
+    ANS: "C"
+    },
+    {Q: "What director has won the most Academy Awards?",
+        A: "John Ford",
+        B: "David Lean",
+        C: "Orson Welles",
+        D: "Steven Spielberg",
+    ANS: "A" // actually A, B and C are correct
+    },
+    {Q: "Who starred in Good Will Hunting?",
+        A: "Charleton Heston",
+        B: "Matt Damon",
+        C: "Harrison Ford",
+        D: "Humphrey Bogart",
+    ANS: "B"
+    }
+];
--- h5-quizzes/quizzes.js
+++ h5-quizzes/quizzes.js
@@ -1,106 +1,29 @@
-var questions = [ 
-    {Q: "Who starred in Casablanca?",
-        A: "Charleton Heston",
-        B: "Matt Damon",
-        C: "Harrison Ford",
-        D: "Humphrey Bogart",
-        ANS: "D"
-    },
-    {Q: "Who won the most Academy Awards (Oscars) for best actress?",
-        A: "Meryl Streep",
-        B: "Emma Thompson",
-        C: "Katharine Hepburn",
-        D: "Ingrid Bergman",
-    ANS: "C"
-    },
-    {Q: "What director has won the most Academy Awards?",
-        A: "John Ford",
-        B: "David Lean",
-        C: "Orson Welles",
-        D: "Steven Spielberg",
-    ANS: "A" // actually A, B and C are correct
-    },
-    {Q: "Who starred in Good Will Hunting?",
-        A: "Charleton Heston",
-        B: "Matt Damon",
-        C: "Harrison Ford",
-        D: "Humphrey Bogart",
-    ANS: "B"
-    }
-];
-
 function addQuizQuestion(question) {
     let clone = $("#quiz-template").clone();
-    clone.find('legend').text(question.Q);
-    $('div label input[value="A"]').closest('label').text(question.A);
-    $('div label input[value="B"]').closest('label').text(question.B);
-    $('div label input[value="C"]').closest('label').text(question.C);
-    $('div label input[value="D"]').closest('label').text(question.D);
+    clone.removeClass('template')
+         .attr('data-ans', question.ANS)
+         .find('legend').text(question.Q);
+    clone.find('div label input[value="A"]').closest('label').append(question.A);
+    clone.find('div label input[value="B"]').closest('label').append(question.B);
+    clone.find('div label input[value="C"]').closest('label').append(question.C);
+    clone.find('div label input[value="D"]').closest('label').append(question.D);
     clone.appendTo('#myquestions');
-    console.log("done");
-}
-
-function addQuestion(el){ //takes one of the elements in the array e.g: questions[0], questions[2]...
-    let fieldset = $("<fieldset>");
-    let legend = $("<legend>").text(el.Q);
-    let divA = $("<div>");
-    let divB = $("<div>");
-    let divC = $("<div>");
-    let divD = $("<div>");
-    let labelA = $("<label>").text(el.A);
-    let labelB = $("<label>").text(el.B);
-    let labelC = $("<label>").text(el.C);
-    let labelD = $("<label>").text(el.D);
-    let options = [divA, divB, divC, divD];
-    let labels = [labelA, labelB, labelC, labelD];
-    options.forEach((_, idx) => options[idx].append(labels[idx]));
-    
-    var inputA = $('<input>', {'type':'radio',
-                            'name':'option',
-                            'value': 'A'});
-    labelA.append(inputA);
-    var inputB = $('<input>', {'type':'radio',
-                            'name':'option',
-                            'value': 'B'});
-    labelB.append(inputB);
-    var inputC = $('<input>', {'type':'radio',
-                            'name':'option',
-                            'value': 'C'});
-    labelC.append(inputC);
-    var inputD = $('<input>', {'type':'radio',
-                            'name':'option',
-                            'value': 'D'});
-    labelD.append(inputD);
-    
-    options.forEach((o) => $(o).on('click', checkCorrect));
-    fieldset.append(legend).attr("data-ans", el.ANS).appendTo('#myquestions');
-    options.forEach((o) => fieldset.append(o));
 }
 
 function checkCorrect(event) {
-    let label = $(event.target).closest('label');
-    let ans = $(label).closest('fieldset').attr('data-ans');
-    let fieldset = $(label).closest('fieldset');
-    if ($(label).find('input').attr('value') === ans) {
+    let fieldset = $(event.target).closest('fieldset');
+    let ans = fieldset.attr('data-ans');
+    let resp = fieldset.find('div label input[type=radio]:checked').val();
+    if (resp === ans) {
         fieldset.attr('class', 'correct');
     } else {
         fieldset.attr('class', 'incorrect')
     }
 }
 
-function addClass(elt, correctness) {
-/*
-Add class, either correct or incorrect to an element
-Args:
-    elt: HTML element, which will be a label for the purposes of the assignment
-    correctness: either "correct" or "incorrect", whichever 
-*/
-    $(elt).attr('class', correctness);
-}
-
-
-questions.forEach(function (qDict) {addQuestion(qDict)}); //this puts each question (no answers yet) in the html
+$("#myquestions").one().on('click', 'button', checkCorrect);
+questions.forEach(function (elt, idx) {addQuizQuestion(elt, idx)}); //this puts each question (no answers yet) in the html
 // we can pass each one of the answers once each question is added
 
 //wrap each questions with their respective answers. Use <form class="question1">?
-addQuizQuestion(questions[0]);
+// addQuizQuestion(questions[0]);
